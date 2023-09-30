7f5eb1ff274f30a4af290a17a64d37dae2bd7735
Adam Naamani <adamnaamani@gmail.com>
2023-09-30 13:23:11 -0700

    Blog

--- bin/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-bundle exec puma -C config/puma.rb
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
