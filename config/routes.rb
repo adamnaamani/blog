da1cdb0972625cc8235a494281aaef099b3bb5ec
Adam Naamani <adamnaamani@gmail.com>
2023-09-30 18:20:37 -0700

    Auth

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
   resources :posts, param: :slug
   resources :subscribers
 
-  get '/404', to: 'errors#not_found'
-  get '/422', to: 'errors#unprocessable'
-  get '/500', to: 'errors#internal_server'
-
   get '/blog', to: 'posts#index'
   get '/:slug', to: 'posts#show'
 end
