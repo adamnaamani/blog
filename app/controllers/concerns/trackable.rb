2b4cec34990cf1697d46116225ac9bce64e5487e
Adam Naamani <adamnaamani@gmail.com>
2023-11-07 20:21:43 -0800

    Production

--- app/controllers/concerns/trackable.rb
+++ app/controllers/concerns/trackable.rb
@@ -2,6 +2,12 @@ module Trackable
   extend ActiveSupport::Concern
 
   included do
+    skip_before_action :track_ahoy_visit, if: :signed_in
+
+    def signed_in
+      user_signed_in?
+    end
+
     def track_action
       return if user_signed_in? && current_user.admin?
 
